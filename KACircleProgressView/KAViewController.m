c68290e5cce9ae64453ea0391a00394002a06972
Kenny <pearapps@gmail.com>
2014-02-06 06:04:32 -0500

    Shows it inside the nav bar - also fixed typo

--- KACircleProgressView/KAViewController.m
+++ KACircleProgressView/KAViewController.m
@@ -18,9 +18,20 @@
 {
     //  KACircleProgressView * c = [[KACircleProgressView alloc] initWithSize:20 withType:KACircleProgressViewTypeCircleBacked andProgressBarLineWidth:1 andCircleBackLineWidth:1];
     KACircleProgressView * c = [[KACircleProgressView alloc] initWithSize:100 withType:KACircleProgressViewTypeCircleBacked andProgressBarLineWidth:7 andCircleBackLineWidth:7];
-    [c setProgress:0.3]; // set progress to 0.1 out of 1.0
+    [c setProgress:0.3]; // set progress to 0.3 out of 1.0
     [self.view addSubview:c];
     [c setCenter:CGPointMake([[UIScreen mainScreen] bounds].size.width/2, [[UIScreen mainScreen] bounds].size.height/2)];
+    
+    
+    
+    
+    KACircleProgressView * barProg = [[KACircleProgressView alloc] initWithSize:30 withType:KACircleProgressViewTypeCircleBacked andProgressBarLineWidth:3 andCircleBackLineWidth:1];
+    [barProg setProgress:0.75]; // set progress to 0.1 out of 1.0
+    [barProg setColorOfProgressBar:[UIColor colorWithRed:0 green:0.4784 blue:255/255.f alpha:1.0]];
+    [barProg setColorOfBackCircle:[UIColor lightGrayColor]];
+    [self.navigationItem setRightBarButtonItem:[[UIBarButtonItem alloc] initWithCustomView:barProg]];
+    
+    
     //[self performSelector:@selector(upDate:) withObject:c afterDelay:0.05];
     [super viewDidLoad];
 }
